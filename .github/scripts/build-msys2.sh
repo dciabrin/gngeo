7abbfff490e6281f494807dafe8d332e62b594ce
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-05-15 14:05:08 +0200

    msys2: move CI build to ucrt64 subsystem
    Also drop to old external NSIS binary installer, as native support
    is now fully provided by MSYS2
    
    Ref dciabrin/ngdevkit#92

--- .github/scripts/build-msys2.sh
+++ .github/scripts/build-msys2.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -eu
+
 autoreconf -iv
 ./configure \
-    --prefix=/mingw64 \
-    --build=x86_64-w64-mingw32 \
-    --host=x86_64-w64-mingw32 \
-    --target=x86_64-w64-mingw32 \
+    --prefix=${MSYSTEM_PREFIX} \
+    --build=${MSYSTEM_CHOST} \
+    --host=${MSYSTEM_CHOST} \
+    --target=${MSYSTEM_CHOST} \
     --program-prefix=ngdevkit- \
     --enable-msys2 \
     --with-glew \
     CFLAGS="-Wno-implicit-function-declaration -DGNGEORC=\\\"ngdevkit-gngeorc\\\"" \
-    GL_LIBS="-L/mingw64/bin -lglew32 -lopengl32"
-make -j1 pkgdatadir=/mingw64/share/ngdevkit-gngeo
-make install pkgdatadir=/mingw64/share/ngdevkit-gngeo
+    GL_LIBS="-L${MSYSTEM_PREFIX}/bin -lglew32 -lopengl32"
+MSYS2_ARG_CONV_EXCL="-DDATA_DIRECTORY=" make pkgdatadir=${MSYSTEM_PREFIX}/share/ngdevkit-gngeo
+make install pkgdatadir=${MSYSTEM_PREFIX}/share/ngdevkit-gngeo
